896887196d76a5b81308bd3d7dc2b538b98f9097
eroux <elie.roux@telecom-bretagne.eu>
2023-02-14 07:10:49 +0100

    bring back code that was removed

--- nlm_classifier_infer.py
+++ nlm_classifier_infer.py
@@ -37,22 +37,55 @@ def batch_data(images, batch_size=8):
     return img_batches
 
 
-def preprocess_image(img):
-    img = tf.io.read_file(img)
-    img = tf.io.decode_jpeg(img, channels=1)
-    
+def get_s3_folder_prefix(wlname, image_group_lname):
+    """
+    gives the s3 prefix (~folder) in which the volume will be present.
+    inpire from https://github.com/buda-base/buda-iiif-presentation/blob/master/src/main/java/
+    io/bdrc/iiif/presentation/ImageInfoListService.java#L73
+    Example:
+       - wlname=W22084, image_group_lname=I0886
+       - result = "Works/60/W22084/images/W22084-0886/
+    where:
+       - 60 is the first two characters of the md5 of the string W22084
+       - 0886 is:
+          * the image group ID without the initial "I" if the image group ID is in the form I\\d\\d\\d\\d
+          * or else the full image group ID (incuding the "I")
+    """
+    md5 = hashlib.md5(str.encode(wlname))
+    two = md5.hexdigest()[:2]
+
+    pre, rest = image_group_lname[0], image_group_lname[1:]
+    if pre == 'I' and rest.isdigit() and len(rest) == 4:
+        suffix = rest
+    else:
+        suffix = image_group_lname
+
+    return 'Works/{two}/{RID}/images/{RID}-{suffix}/'.format(two=two, RID=wlname, suffix=suffix)
+
+def gets3blob(s3Key):
+    f = io.BytesIO()
+    try:
+        S3.download_fileobj('archive.tbrc.org', s3Key, f)
+        return f
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            return None
+        else:
+            raise
+
+def preprocess_image(s3prefix, imgfname):
+    img = Image.open(gets3blob(s3prefix+imgfname)).convert('L')
+    img = tf.keras.utils.img_to_array(img)
     if img.shape[0] <= img.shape[1]:
         img = tf.slice(img, begin=[0, 0, 0], size=[tf.shape(img)[0], tf.shape(img)[0], 1])
     else:
         img = tf.slice(img, begin=[0, 0, 0], size=[tf.shape(img)[0], tf.shape(img)[0] // 2, 1])
-       
     img = tf.image.resize_with_pad(
         img,
         target_height=IMAGE_SIZE,
         target_width=IMAGE_SIZE,
         method=tf.image.ResizeMethod.LANCZOS3,
     )
-    
     img = tf.cast(img, tf.float32)
     img /= 255.0
 
